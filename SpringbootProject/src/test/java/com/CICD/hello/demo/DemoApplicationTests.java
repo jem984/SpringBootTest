1ac351f319df1c181bc11a4e8919f9d48eb364cb
Jeremy Seow <jeremyseow984@gmail.com>
2018-10-13 11:55:05 +0800

    Repo init

---
+++ SpringbootProject/src/test/java/com/CICD/hello/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.CICD.hello.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testCase1() {
+        assert (true);
+    }
+
+}
